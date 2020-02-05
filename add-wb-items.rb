eb74fda5b743f59fd63a8b76271e9b6b7e25f9e1
Eurus Wagner <euruswagner@gmail.com>
2020-02-05 21:43:38 +0000

    Added World Boss Items

---
+++ add-wb-items.rb
@@ -0,0 +1,75 @@
+# Melee Weapons is 1
+# Melee Accessories is 2
+# Melee Non Set Pieces is 3
+# Caster Weapons is 4
+# Caster Accessories is 5
+# Caster Non Set Pieces is 6
+# Healer Weapons is 7
+# Healer Accessories is 8
+# Healer Non Set Pieces is 9
+# Miscellaneous is 10
+# Tier 1 Set Pieces is 11
+# Tier 2 Set Pieces is 12
+
+# Melee Weapons World Bosses (1)
+Item.create(name: 'Empyrean Demolisher',
+            category_id: 1)
+
+Item.create(name: 'Typhoon',
+            category_id: 1)
+
+Item.create(name: 'Eskhandar\'s Left Claw',
+            category_id: 1)
+
+# Melee Accessories World Bosses (2)
+Item.create(name: 'Blazefury Medallion',
+            category_id: 2)
+
+# Melee Non Set Pieces World Bosses (3)
+Item.create(name: 'Doomhide Gauntlets',
+            category_id: 3)
+
+Item.create(name: 'Eskhandar\'s Belt',
+            category_id: 3)
+
+Item.create(name: 'Unmelting Ice Girdle',
+            category_id: 3)
+
+Item.create(name: 'Puissant Cape',
+            category_id: 3)
+
+# Caster Weapons World Bosses (4)
+Item.create(name: 'Amberseal Keeper',
+            category_id: 4)
+
+Item.create(name: 'Cold Snap',
+            category_id: 4)
+
+Item.create(name: 'Fang of the Mystics',
+            category_id: 4)
+
+# Caster Accessories World Bosses (5)
+Item.create(name: 'Ring of Entropy',
+            category_id: 5)
+
+# Caster Non Set Pieces World Bosses (6)
+Item.create(name: 'Infernal Headcage',
+            category_id: 6)
+
+Item.create(name: 'Flayed Doomguard Belt',
+            category_id: 6)
+
+Item.create(name: 'Leggings of Arcane Supremacy',
+            category_id: 6)
+
+Item.create(name: 'Snowblind Shoes',
+            category_id: 6)
+
+# Healer Weapons World Bosses (7)
+# Healer Accessories World Bosses (8)
+# Healer Non Set Pieces World Bosses (9)
+Item.create(name: 'Crystal Adorned Crown',
+            category_id: 9)
+
+Item.create(name: 'Drape of Benediction',
+            category_id: 9)
