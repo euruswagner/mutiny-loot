c8a1dae26c59849cfe8d025bf36dd33cb71b2942
Eurus Wagner <euruswagner@gmail.com>
2020-03-07 20:23:08 +0000

    Added bit extra to account in delay between start of raid and attendance being entered

--- app/models/raider.rb
+++ app/models/raider.rb
@@ -5,6 +5,6 @@ class Raider < ApplicationRecord
 
   def weeks_with_the_guild?
     return 0 if self.attendances.first.nil?
-    return ((Time.now - self.attendances.first.created_at)/60/60/24/7) + 1.3
+    return ((Time.now - self.attendances.first.created_at)/60/60/24/7) + 1.36
   end  
 end
