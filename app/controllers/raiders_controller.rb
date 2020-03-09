3cf43ceca104a3f2c79e170889a1cc77643882f8
Eurus Wagner <euruswagner@gmail.com>
2020-03-09 16:36:33 +0000

    Back end for creating raiders

--- app/controllers/raiders_controller.rb
+++ app/controllers/raiders_controller.rb
@@ -1,5 +1,6 @@
 class RaidersController < ApplicationController
-  before_action :authenticate_user!, only: [:create]
+  before_action :authenticate_user!, only: [:create, :edit]
+  before_action :authenticate_admin!, only: [:create]
 
   def index
     @raiders = Raider.all
@@ -10,6 +11,18 @@ class RaidersController < ApplicationController
   end
 
   def create
-    Raider.create
+    Raider.create(raider_params)
+  end
+
+  private
+
+  def raider_params
+    params.require(:raider).permit(:name, :which_class, :role)
+  end
+
+  def authenticate_admin!
+    if current_user.admin != true
+      redirect_to root_path, alert: 'You do not have the privileges required to do that.'
+    end
   end
 end
--- spec/controllers/raiders_controller_spec.rb
+++ spec/controllers/raiders_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe RaidersController, type: :controller do
+  describe 'raiders#create action' do
+    it 'allows raiders to be created' do
+      user = FactoryBot.create(:user, admin: true)
+      sign_in user
 
+      post :create, params: {raider: {name: 'Test', which_class: 'Shaman', role: 'Elemental'}}
+
+      last_raider = Raider.last
+      expect(last_raider.name).to eq 'Test'
+      expect(last_raider.which_class).to eq 'Shaman'
+      expect(last_raider.role).to eq 'Elemental'
+    end
+
+    it 'does not allow non admins to create raiders' do
+      user = FactoryBot.create(:user, admin: false)
+      sign_in user
+
+      post :create, params: {raider: {name: 'Test', which_class: 'Shaman', role: 'Elemental'}}
+
+      expect(response).to redirect_to root_path
+      expect(Raider.count).to eq 0
+    end
+  end
 end
