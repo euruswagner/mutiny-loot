f86c088943a6d5cf5e634528d2e6aefe685f84c5
Eurus Wagner <euruswagner@gmail.com>
2020-04-07 16:55:18 -0400

    Update route.rb
    Eliminated commented out root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "users/registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'categories#index'
   root 'pages#frontpage'
   resources :categories, only: [:show, :index]
   resources :items, only: [:show, :edit, :update] do 
