ae83beb58cf485defd3d08c59b46c55b210e58f1
Eurus Wagner <euruswagner@gmail.com>
2020-04-18 03:08:35 +0000

    Deleted method that I moved to controller

--- app/models/raid.rb
+++ app/models/raid.rb
@@ -10,27 +10,6 @@ class Raid < ApplicationRecord
     return false
   end
   
-  def organized_signups
-    tanks = []
-    healers = []
-    dps_warriors = []
-    rogues = []
-    ranged = []
-    raid_id = self.id
-    signups = Signup.where(raid_id: raid_id)
-    signups.each do |signup|
-      tanks << signup if signup.user.raider.name == 'Ezpzlul' && tanks.count <= 2 && self.zg?
-      tanks << signup if signup.user.raider.role == 'Tank' && tanks.count <= 2 && self.zg?
-      tanks << signup if signup.user.raider.role == 'Tank'
-      healers << signup if signup.user.raider.role == 'Healer' && healers.count <= 5 && self.zg?
-      healers << signup if signup.user.raider.role == 'Healer'
-      next
-    end
-    melee = dps_warriors + rogues
-    raid = [tanks, healers, melee, ranged]
-    return raid
-  end
-
   validates :name, presence: true, length: {minimum: 2, maximum: 25}
   validates :start_time, presence: true
 end
